9da2b07270c43c3868ec0078ddc2210e0ee06286
JulioYauri <83053778+JulioYauri@users.noreply.github.com>
2022-11-25 22:55:19 -0500

    Add files via upload

---
+++ webdriverdemo/src/test/java/webdriverdemo/webdriverdemo.java
@@ -0,0 +1,42 @@
+package webdriverdemo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class webdriverdemo {
+   public static void main(String[] args) {
+   
+      WebDriver driver = new FirefoxDriver();
+      //Puts an Implicit wait, Will wait for 10 seconds before throwing exception
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      
+      //Launch website
+      driver.navigate().to("https://www.calculator.net/percent-calculator.html");
+      
+      //Maximize the browser
+      driver.manage().window().maximize();
+      
+      // Enter value 10 in the first number of the percent Calculator
+      driver.findElement(By.id("cpar1")).sendKeys("10");
+      
+      // Enter value 50 in the second number of the percent Calculator
+      driver.findElement(By.id("cpar2")).sendKeys("50");
+      
+      // Click Calculate Button
+      driver.findElement(By.xpath(".//*[@id = 'content']/table/tbody/tr[2]/td/input[2]")).click();
+
+      
+      // Get the Result Text based on its xpath
+      String result =
+         driver.findElement(By.xpath(".//*[@id = 'content']/p[2]/font/b")).getText();
+
+      
+      // Print a Log In message to the screen
+      System.out.println(" The Result is " + result);
+      
+      //Close the Browser.
+      driver.close();
+   }
+}
